3e06dc80582430040adbf03935f368e78cc21c86
yoondev83 <yoondev83@gmail.com>
2021-09-30 20:27:42 +0900

    Not Found page added

--- src/App.js
+++ src/App.js
@@ -9,22 +9,22 @@ import WritingForm from './components/Board/Writing/WritingForm';
 import Post from './components/Board/Post';
 import SignInMain from './components/SignIn/SignInMain';
 import SignUp from './components/SignUp/SignUp';
+import NotFound from './components/NotFound';
 function App() {
 
   return (
     <div className={classes.App}>
       <CssBaseline/>
         <Router>
-          <Route path="/" exact component={Main}/>
           <Switch>
-            <Route path="/board" component={BoardList}/>
-            <Route path="/post" component={Post}/>
-            <Route path="/graph" component={GraphMain}/>
-            {/* <Route path="/graph/:graphName" render={() => <StackedBarplotPerOffense data={data}/>}/> */}
-            <Route path="/write" component={WritingForm}/>
-            <Route path="/login" component={SignInMain}/>
+            <Route path="/" exact component={Main}/>
+            <Route path="/board"  component={BoardList}/>
+            <Route path="/post"   component={Post}/>
+            <Route path="/graph"  component={GraphMain}/>
+            <Route path="/write"  component={WritingForm}/>
+            <Route path="/login"  component={SignInMain}/>
             <Route path="/signUp" component={SignUp}/>
-            {/* <Route component={NotFound} /> */}
+            <Route component={NotFound} />
           </Switch>
         </Router>
     </div>
